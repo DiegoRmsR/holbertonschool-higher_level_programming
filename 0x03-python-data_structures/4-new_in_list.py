6c60f4271b53ed086bf26b55fd3df32a2f8ceab6
DiegoRmsR <51675197+DiegoRmsR@users.noreply.github.com>
2019-09-17 16:29:28 -0500

    Fix: delete 3 and add -1

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
-    length = len(my_list[:3])
-    if idx < 0 or idx > length:
+    length = len(my_list)
+    if idx < 0 or idx > length - 1:
         return new_list
     new_list[idx] = element
     return new_list
