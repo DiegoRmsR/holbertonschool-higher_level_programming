6798adbd922899a11ca916792700f16e3b7e4f12
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-10-07 17:10:21 +0000

    Update: pep8

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-"""
-class Rectangle that defines a rectangle
-"""
 class Rectangle:
-    """  """
+    """ class Rectangle that defines a rectangle """
     def __init__(self, width=0, height=0):
         """ instance method """
         self.width = width
