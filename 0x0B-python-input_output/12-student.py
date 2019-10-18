2890215a85fd62e7ace94d65d73d9a8212f0dea0
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-10-18 04:56:27 +0000

    Add: class Student that defines a student by: (based on 11-student.py)

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -6,11 +6,19 @@ class Student
 
 class Student:
 
-	def __init__(self, first_name, last_name, age):
-	    self.first_name = first_name
-            self.last_name = last_name
-    	    self.age = age
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-	def to_json(self, attrs=None):
-	    if type(attrs) is not list:
-                return (self.__dict__)
+    def to_json(self, attrs=None):
+        if type(attrs) is not list:
+            return (self.__dict__)
+        else:
+            new_d = {}
+            obj = self.__dict__
+            for i in attrs:
+                for j in obj:
+                    if (i == j):
+                        new_d[j] = obj[j]
+            return (new_d)
