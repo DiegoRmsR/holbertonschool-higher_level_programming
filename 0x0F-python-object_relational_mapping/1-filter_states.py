2e80ecb68c1ec1da2d140390267cac5d8dfe685c
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-12-16 05:29:53 +0000

    [Feature] pep8

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-script that lists all states with a name starting with N (upper N) from the databe
+script that lists all states with a name starting with
+N (upper N) from the databe
 """
 import sys
 import MySQLdb
@@ -10,7 +11,8 @@ if __name__ == "__main__":
                          user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3])
     query = db.cursor()
-    query.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    query.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'\
+                  ORDER BY id ASC")
     states = query.fetchall()
     for state in states:
         print(state)
