0c66d4fa6f7bc8eaee30a88990021d55a7be2aac
DiegoRmsR <Diegoramos.mt@gmail.com>
2020-01-13 20:48:19 -0500

    [Feature] Python script that takes in a URL and an email, sends a POST request to the passed URL

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL and an email,
+sends a POST request to the passed URL """
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == '__main__':
+    values = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print(the_page.decode('utf-8'))
