ecb7f21c84fa78a981beef872901e06d66eb10b2
DiegoRmsR <Diegoramos.mt@gmail.com>
2020-01-14 00:33:18 -0500

    [Feature] Python script that takes in a URL, sends a request to the URL and displays the body of the response.

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request
+to the URL and displays the body of the response.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    bad_r = requests.get(sys.argv[1])
+    print("Error code: {}".format(bad_r.status_code))
