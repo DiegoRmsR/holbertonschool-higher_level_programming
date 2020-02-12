db39e0955c16c10bd4396287ab090e4330ea94e2
DiegoRmsR <Diegoramos.mt@gmail.com>
2020-02-12 00:49:31 -0500

    [Feature] script that computes the number of tasks completed by user id

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const request = require('request');
+
+request(process.argv[2], function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const newdict = {};
+    const jsonb = JSON.parse(body);
+    for (let i = 0; i < json.length; i++) {
+      if (jsonb[i].completed === true) {
+	if (jsonb[i].userId in newdict) {
+	  newdict[jsonb[i].userId] += 1;
+        } else {
+          newdict[jsonb[i].userId] = 0;
+        }
+      }
+    }
+    console.log(newdict);
+  }
+});
+	  
