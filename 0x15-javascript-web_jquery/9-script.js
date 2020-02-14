a7d6f7c3ec024f15f9228f54f40ee485335db70b
DiegoRmsR <Diegoramos.mt@gmail.com>
2020-02-13 21:46:33 -0500

    [Feature] delete

--- 0x15-javascript-web_jquery/9-script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  $.getJSON('https://swapi.co/api/films/?format=json', function (result) {
-    for (const movie of result.results) {
-      $('ul#list_movies').append("<li>" + movie.title + "</li>");
-    }
-  });
-});
